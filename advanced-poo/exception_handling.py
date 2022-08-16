757f0e4359cd21418285dd97da067aefabef7840
Henrique Albuquerque <henrique_barretto@outlook.com>
2022-08-16 09:43:49 -0300

    desafio: do not let it divide by zero

--- advanced-poo/exception_handling.py
+++ advanced-poo/exception_handling.py
@@ -10,3 +10,15 @@ finally:
     print("Finally will run independently of the occurs of errors")
 # else:
 #     print("Else block here, runs if everything runs without error")
+
+
+def divider(a, b):
+    # TODO
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        print('Please do not divide by zero!')
+
+
+print(divider(2, 0))
