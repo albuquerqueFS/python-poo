2bafb47c5bd132609bcd5eb87dfc1e6f49dcd7b3
Henrique Albuquerque <henrique_barretto@outlook.com>
2022-08-21 20:49:32 -0300

    dynamic url parameters

--- project_views/first_app/views.py
+++ project_views/first_app/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'
+}
+
 # Create your views here.
-def simple_view(request):
-    return HttpResponse('A simple view')
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    result = num1 + num2
+    return HttpResponse(str(result))
