528aaf11984c61081bcd4be7204e4e4fd993b0b2
Henrique Albuquerque <albuquerque.henrique@bcg.com>
2022-08-27 00:54:33 -0300

    project with admin settings setted

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+
+class MyCarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Time information', { 'fields': ['year']}),
+        ('Car information', { 'fields': ['brand']})
+    ]
+
+admin.site.register(Car, MyCarAdmin)
